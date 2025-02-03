21aa9518759ed689f3c5833a719e8b286b1796d0
박주현 <leehot2665@naver.com>
2025-02-03 21:29:11 +0900

    백준 4375번 문제 실2

---
+++ Math/4375/Main4375.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main4375 {
+
+    public static void main(String[] args) throws Exception {
+        //2와 5로 나누어 떨어지지 않는 정수 n(1 ≤ n ≤ 10000)가 주어졌을 때,
+        // 각 자릿수가 모두 1로만 이루어진 n의 배수를 찾는 프로그램을 작성하시오.
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+            int input = sc.nextInt();
+            int num = 1;
+            int count = 1;
+            while (true) {
+                if (num % input != 0) {
+                    num = (num * 10 + 1) % input;
+                    count++;
+                } else break;
+
+            }
+            System.out.println(count);
+        }
+    }
+}
